eef75560e4516b059e58efdf99a074f5b36a1370
Siriratda <144195995+Siriratda@users.noreply.github.com>
2024-10-17 16:26:05 +0700

    Update init.js

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 db = new Mongo().getDB("mydatabase");
 
 db.students.insertMany([
-    { studentCode: "S001", firstName: "John", lastName: "Doe", age: 21, email: "john@example.com" },
-    { studentCode: "S002", firstName: "Jane", lastName: "Doe", age: 22, email: "jane@example.com" }
+    { studentCode: "d003", firstName: "Siriratda", lastName: "Wangsatang", age: 21, email: "Fareeda@gmail.com" },
+    { studentCode: "d004", firstName: "Teerapop", lastName: "Pechnin", age: 22, email: "teepop@gmail.com" },
+    { studentCode: "d005", firstName: "da", lastName: "wang", age: 23, email: "da@gmail.com" }
 ]);
